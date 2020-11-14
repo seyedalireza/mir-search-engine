7ab20ba2d4dde4f8b47f297ea450f5121f3467cd
sajjad2014 <sajjadshahabi20@yahoo.com>
2020-11-14 11:45:28 +0330

    add bigram

--- Index/Indexer.py
+++ Index/Indexer.py
@@ -3,16 +3,29 @@ from Index.WordIndex import WordIndex
 
 class Indexer:
     def __init__(self):
-        self.word_list = []
+        self.word_dict = {}
+        self.bigram = {}
 
     def create_index(self, word_list):
         for n_word in word_list:
-            found_word = None
-            for word in self.word_list:
-                if word.word == n_word.word:
-                    found_word = word
-            if found_word is None:
-                self.word_list.insert(len(self.word_list), WordIndex(n_word.word))
-                self.word_list[len(self.word_list) - 1].add_new_word(n_word)
+            if n_word.word in self.word_dict:
+                self.word_dict[n_word.word].add_new_word(n_word)
             else:
-                found_word.add_new_word(n_word)
+                self.word_dict[n_word.word] = WordIndex(n_word.word)
+                self.word_dict[n_word.word].add_new_word(n_word)
+                self.add_bigram(n_word.word)
+
+    def add_bigram(self, word):
+        con_word = "$" + word + "$"
+        for i in range(len(con_word) - 1):
+            bigram = con_word[i:i+2]
+            if bigram in self.bigram:
+                if word not in self.bigram[bigram]:
+                    for j in range(len(self.bigram[bigram])):
+                        if self.bigram[bigram][j] > word:
+                            self.bigram[bigram].insert(j, word)
+                            return
+                    self.bigram[bigram].append(word)
+            else:
+                self.bigram[bigram] = []
+                self.bigram[bigram].append(word)
